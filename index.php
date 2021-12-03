052f3b152b720eb43bfd7a610b07c109223cfaae
Emalios <35004924+Emalios@users.noreply.github.com>
2021-12-03 14:59:24 +0100

    connexion à éloquent

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Capsule\Manager as DB;
+
+$app = new \Slim\App;
+$app->get('/games/{id}',
+ function (Request $req, Response $resp, $args) {
+    print("cc");
+ });
+$app->post('/games/{id}',
+ function (Request $req, Response $resp, $args) {
+    print("id");
+ });
+$app->run();
+
+$db = new DB();
+$db->addConnection( [
+ 'driver' => 'mysql',
+ 'host' => 'webetu.univ-lorraine.fr',
+ 'database' => 'database',
+ 'username' => 'user',
+ 'password' => 'password',
+ 'charset' => 'utf8',
+ 'collation' => 'utf8_unicode_ci',
+ 'prefix' => ''
+] );
+$db->setAsGlobal();
+$db->bootEloquent();
