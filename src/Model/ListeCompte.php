4fcf6ec8df49da96ad8b47b03410303a6343f3b6
arthur21122 <arthur.cappellina6@etu.univ-lorraine.fr>
2022-01-22 12:22:07 +0100

    Ajout methode pour savoir si un utilisateur a participé a une liste

--- src/Model/ListeCompte.php
+++ src/Model/ListeCompte.php
@@ -6,8 +6,14 @@ class ListeCompte extends \Illuminate\Database\Eloquent\Model {
     
     protected $table = 'listecompte';
 
-    protected $primaryKey = "loginCompte";
+    protected $primaryKey = "idCompte";
 
     public $timestamps = false;
+
+    public static function getParticipation($idListe) {
+        $participation = ListeCompte::where("idCompte", "=", $_SESSION["userId"])->where("idListe", "=", $idListe)->first();
+        echo $participation;
+        return $participation ?? null; 
+    }
 
 }
